79f7e04680ad2a5b386e89b46c7a59e2b668f706
jovenan <jovenan2001@gmail.com>
2024-08-10 13:18:32 -0300

    feat: add mobile cards section

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -22,26 +22,27 @@ import * as $$$$$$5 from "./sections/Content/Hero.tsx";
 import * as $$$$$$6 from "./sections/Content/HeroFull.tsx";
 import * as $$$$$$7 from "./sections/Content/Intro.tsx";
 import * as $$$$$$8 from "./sections/Content/Logos.tsx";
-import * as $$$$$$9 from "./sections/Footer/Footer.tsx";
-import * as $$$$$$10 from "./sections/Header/Header.tsx";
-import * as $$$$$$11 from "./sections/Images/Banner.tsx";
-import * as $$$$$$12 from "./sections/Images/Carousel.tsx";
-import * as $$$$$$13 from "./sections/Images/ImageGallery.tsx";
-import * as $$$$$$14 from "./sections/Images/ShoppableBanner.tsx";
-import * as $$$$$$15 from "./sections/Links/LinkTree.tsx";
-import * as $$$$$$16 from "./sections/Miscellaneous/CampaignTimer.tsx";
-import * as $$$$$$17 from "./sections/Miscellaneous/CookieConsent.tsx";
-import * as $$$$$$18 from "./sections/Newsletter/Newsletter.tsx";
-import * as $$$$$$19 from "./sections/Product/ProductDetails.tsx";
-import * as $$$$$$20 from "./sections/Product/ProductShelf.tsx";
-import * as $$$$$$21 from "./sections/Product/ProductShelfTabbed.tsx";
-import * as $$$$$$22 from "./sections/Product/SearchResult.tsx";
-import * as $$$$$$23 from "./sections/Product/ShelfWithImage.tsx";
-import * as $$$$$$24 from "./sections/Product/Wishlist.tsx";
-import * as $$$$$$25 from "./sections/Session.tsx";
-import * as $$$$$$26 from "./sections/Social/InstagramPosts.tsx";
-import * as $$$$$$27 from "./sections/Social/WhatsApp.tsx";
-import * as $$$$$$28 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$9 from "./sections/Content/MobileCards.tsx";
+import * as $$$$$$10 from "./sections/Footer/Footer.tsx";
+import * as $$$$$$11 from "./sections/Header/Header.tsx";
+import * as $$$$$$12 from "./sections/Images/Banner.tsx";
+import * as $$$$$$13 from "./sections/Images/Carousel.tsx";
+import * as $$$$$$14 from "./sections/Images/ImageGallery.tsx";
+import * as $$$$$$15 from "./sections/Images/ShoppableBanner.tsx";
+import * as $$$$$$16 from "./sections/Links/LinkTree.tsx";
+import * as $$$$$$17 from "./sections/Miscellaneous/CampaignTimer.tsx";
+import * as $$$$$$18 from "./sections/Miscellaneous/CookieConsent.tsx";
+import * as $$$$$$19 from "./sections/Newsletter/Newsletter.tsx";
+import * as $$$$$$20 from "./sections/Product/ProductDetails.tsx";
+import * as $$$$$$21 from "./sections/Product/ProductShelf.tsx";
+import * as $$$$$$22 from "./sections/Product/ProductShelfTabbed.tsx";
+import * as $$$$$$23 from "./sections/Product/SearchResult.tsx";
+import * as $$$$$$24 from "./sections/Product/ShelfWithImage.tsx";
+import * as $$$$$$25 from "./sections/Product/Wishlist.tsx";
+import * as $$$$$$26 from "./sections/Session.tsx";
+import * as $$$$$$27 from "./sections/Social/InstagramPosts.tsx";
+import * as $$$$$$28 from "./sections/Social/WhatsApp.tsx";
+import * as $$$$$$29 from "./sections/Theme/Theme.tsx";
 
 const manifest = {
   "loaders": {
@@ -61,26 +62,27 @@ const manifest = {
     "site/sections/Content/HeroFull.tsx": $$$$$$6,
     "site/sections/Content/Intro.tsx": $$$$$$7,
     "site/sections/Content/Logos.tsx": $$$$$$8,
-    "site/sections/Footer/Footer.tsx": $$$$$$9,
-    "site/sections/Header/Header.tsx": $$$$$$10,
-    "site/sections/Images/Banner.tsx": $$$$$$11,
-    "site/sections/Images/Carousel.tsx": $$$$$$12,
-    "site/sections/Images/ImageGallery.tsx": $$$$$$13,
-    "site/sections/Images/ShoppableBanner.tsx": $$$$$$14,
-    "site/sections/Links/LinkTree.tsx": $$$$$$15,
-    "site/sections/Miscellaneous/CampaignTimer.tsx": $$$$$$16,
-    "site/sections/Miscellaneous/CookieConsent.tsx": $$$$$$17,
-    "site/sections/Newsletter/Newsletter.tsx": $$$$$$18,
-    "site/sections/Product/ProductDetails.tsx": $$$$$$19,
-    "site/sections/Product/ProductShelf.tsx": $$$$$$20,
-    "site/sections/Product/ProductShelfTabbed.tsx": $$$$$$21,
-    "site/sections/Product/SearchResult.tsx": $$$$$$22,
-    "site/sections/Product/ShelfWithImage.tsx": $$$$$$23,
-    "site/sections/Product/Wishlist.tsx": $$$$$$24,
-    "site/sections/Session.tsx": $$$$$$25,
-    "site/sections/Social/InstagramPosts.tsx": $$$$$$26,
-    "site/sections/Social/WhatsApp.tsx": $$$$$$27,
-    "site/sections/Theme/Theme.tsx": $$$$$$28,
+    "site/sections/Content/MobileCards.tsx": $$$$$$9,
+    "site/sections/Footer/Footer.tsx": $$$$$$10,
+    "site/sections/Header/Header.tsx": $$$$$$11,
+    "site/sections/Images/Banner.tsx": $$$$$$12,
+    "site/sections/Images/Carousel.tsx": $$$$$$13,
+    "site/sections/Images/ImageGallery.tsx": $$$$$$14,
+    "site/sections/Images/ShoppableBanner.tsx": $$$$$$15,
+    "site/sections/Links/LinkTree.tsx": $$$$$$16,
+    "site/sections/Miscellaneous/CampaignTimer.tsx": $$$$$$17,
+    "site/sections/Miscellaneous/CookieConsent.tsx": $$$$$$18,
+    "site/sections/Newsletter/Newsletter.tsx": $$$$$$19,
+    "site/sections/Product/ProductDetails.tsx": $$$$$$20,
+    "site/sections/Product/ProductShelf.tsx": $$$$$$21,
+    "site/sections/Product/ProductShelfTabbed.tsx": $$$$$$22,
+    "site/sections/Product/SearchResult.tsx": $$$$$$23,
+    "site/sections/Product/ShelfWithImage.tsx": $$$$$$24,
+    "site/sections/Product/Wishlist.tsx": $$$$$$25,
+    "site/sections/Session.tsx": $$$$$$26,
+    "site/sections/Social/InstagramPosts.tsx": $$$$$$27,
+    "site/sections/Social/WhatsApp.tsx": $$$$$$28,
+    "site/sections/Theme/Theme.tsx": $$$$$$29,
   },
   "actions": {
     "site/actions/minicart/submit.ts": $$$$$$$$$0,
---
+++ sections/Content/MobileCards.tsx
@@ -0,0 +1,42 @@
+import type { HTMLWidget, ImageWidget } from "apps/admin/widgets.ts";
+import Image from "apps/website/components/Image.tsx";
+
+export interface Card {
+  image: ImageWidget;
+  description: string;
+  href: string;
+}
+
+export interface Props {
+  cards: Card[];
+}
+
+export default function MobileCards({
+  cards = [],
+}: Props) {
+  return (
+    <div class="lg:hidden my-[40px] px-[32px] flex gap-[2rem] flex-wrap justify-between	">
+      {cards.map((card) => (
+        <div class="flex flex-col w-full max-w-[44%]">
+          <a href={card.href} class="w-full h-full">
+            <Image
+              width={160}
+              class="w-full object-fit"
+              sizes="(max-width: 640px) 100vw, 30vw"
+              src={card.image}
+              alt={card.image}
+              decoding="async"
+              loading="lazy"
+            />
+          </a>
+          <a
+            class="my-[.5rem] flex font-bold text-[#000] leading-[1.5] text-[16px]"
+            href={card.href}
+          >
+            {card.description}
+          </a>
+        </div>
+      ))}
+    </div>
+  );
+}
