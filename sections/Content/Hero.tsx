e0d52a399c07b6841730d353737ef90a8cfc8428
jovenan <jovenan2001@gmail.com>
2024-08-10 12:46:27 -0300

    feat: hero styles

--- sections/Content/Hero.tsx
+++ sections/Content/Hero.tsx
@@ -12,6 +12,7 @@ export interface Props {
   title: HTMLWidget;
   description: string;
   image?: ImageWidget;
+  mobileImage?: ImageWidget;
   placement: "left" | "right";
   cta: CTA[];
 }
@@ -25,23 +26,24 @@ export default function HeroFlats({
   title = "Hero",
   description = "Your description here",
   image,
+  mobileImage,
   placement,
   cta,
 }: Props) {
   return (
     <div>
-      <div class="mx-auto flex flex-col items-center gap-8">
+      <div class="mx-auto flex flex-col items-center gap-[5rem] bg-[#000]">
         <div
-          class={`flex w-full xl:container xl:mx-auto py-20 mx-5 md:mx-10 z-10 ${
+          class={`flex w-full xl:container xl:mx-auto z-10 ${
             image
               ? PLACEMENT[placement]
               : "flex-col items-center justify-center text-center"
-          } lg:py-36 gap-12 md:gap-20 items-center`}
+          } items-end`}
         >
           {image && (
             <Image
               width={640}
-              class="w-full lg:w-1/2 object-fit"
+              class="hidden lg:flex lg:w-[60%] object-fit"
               sizes="(max-width: 640px) 100vw, 30vw"
               src={image}
               alt={image}
@@ -49,31 +51,43 @@ export default function HeroFlats({
               loading="lazy"
             />
           )}
+          {mobileImage && (
+            <Image
+              width={640}
+              class="w-full lg:hidden object-fit"
+              sizes="(max-width: 640px) 100vw, 30vw"
+              src={mobileImage}
+              alt={mobileImage}
+              decoding="async"
+              loading="lazy"
+            />
+          )}
           <div
-            class={`mx-6 lg:mx-auto lg:w-full space-y-4 gap-4 ${
-              image
-                ? "lg:w-1/2 lg:max-w-xl"
-                : "flex flex-col items-center justify-center lg:max-w-3xl"
+            class={`hidden lg:flex px-[5rem] pb-[5rem] lg:w-[40%] space-y-4 gap-4 ${
+              image ? "lg:w-1/2" : "flex flex-col items-center justify-end"
             }`}
           >
             <div
-              class="inline-block text-[80px] leading-[100%] font-medium tracking-[-2.4px]"
+              class="inline-block text-[5rem] leading-[100%] font-black text-[#fff]"
               dangerouslySetInnerHTML={{
                 __html: title,
               }}
             >
             </div>
-            <p class="text-zinc-400 text-[16px] md:text-[18px] leading-[150%]">
+            <p class="text-white text-[16px] md:text-[18px] leading-[150%]">
               {description}
             </p>
-            <div class="flex flex-col items-center lg:items-start lg:flex-row gap-4">
+            <div
+              class="flex flex-col items-center lg:items-start lg:flex-row gap-4 w-full"
+              style={{ marginTop: "32px" }}
+            >
               {cta?.map((item) => (
                 <a
                   key={item?.id}
                   id={item?.id}
                   href={item?.href}
                   target={item?.href.includes("http") ? "_blank" : "_self"}
-                  class={`group relative overflow-hidden rounded-full hover:bg-gradient-to-r px-6 py-2 lg:px-8 lg:py-3 transition-all duration-300 ease-out ${
+                  class={`group w-full relative overflow-hidden text-center rounded-[.25rem] text-[16px] hover:bg-gradient-to-r py-[.75rem] transition-all duration-300 ease-out ${
                     item.variant === "Reverse"
                       ? "bg-secondary text-white"
                       : "bg-accent text-black"
@@ -81,7 +95,7 @@ export default function HeroFlats({
                 >
                   <span class="ease absolute right-0 -mt-12 h-32 w-8 translate-x-12 rotate-12 transform bg-white opacity-10 transition-all duration-1000 group-hover:-translate-x-40">
                   </span>
-                  <span class="relative font-medium lg:text-[20px]">
+                  <span class="relative font-[600] text-[16px] text-center">
                     {item?.text}
                   </span>
                 </a>
